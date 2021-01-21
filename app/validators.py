a2d6a04fbe230e45ad6329e902805a75c2fd740c
WLAjansen <40787982+WLAjansen@users.noreply.github.com>
2021-01-21 13:24:04 +0100

    major update
    Alle bestanden van de applicatie tot nu toe.

--- app/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def file_size(value):
-   filesize=value.size 
-   if filesize>419430400:
-    #    Verander het limiet van de grote van het bestand hieronder
-       raise ValidationError("maximum size is 50 mb")
